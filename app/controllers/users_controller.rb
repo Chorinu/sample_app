2460ce0b5959d0e6584b0a3b4643601b7a428624
Kanako Baba <a2233kb@aiit.ac.jp>
2022-05-10 12:02:06 +0000

    Finish ch07

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,41 @@
 class UsersController < ApplicationController
-  def new
+  # GET /users/:id
+  def show
+    @user = User.find(params[:id])
   end
+  
+  # GET /users/new
+  def new
+    @user = User.new
+  end
+  
+  #POST/users (+ params)
+  def create
+    @user = User.new(user_params)
+      # 実装は終わっていないことに注意!
+    #(user + given params).save
+    if @user.save
+      # 保存の成功をここで扱う。
+      
+      flash[:success] = "Welcome to the Sample App!"
+      
+      #GET "/users/#{@user.id}"
+      redirect_to @user
+      # redirect_to user_path(@user)
+      # redirect_to user_path(@user.id)
+      # redirect_to user_path(1)
+      #             => /users/1
+      
+    else
+      render 'new'
+    end
+  end
+  
+  private
+  
+  def user_params
+      params.require(:user).permit(:name, :email, :password,
+                                   :password_confirmation)
+  end
+  
 end
