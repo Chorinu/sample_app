1c0df088cbfbe6eb8863e953965ab40610d26739
Kanako Baba <a2233kb@aiit.ac.jp>
2022-06-03 02:20:51 +0000

    Finish ch09

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -4,15 +4,37 @@ module SessionsHelper
   def log_in(user)
     session[:user_id] = user.id
   end
-
-  # 現在ログイン中のユーザーを返す（いる場合）
+  
+  # ユーザーのセッションを永続的にする
+  def remember(user)
+    user.remember
+    cookies.permanent.encrypted[:user_id] = user.id
+    cookies.permanent[:remember_token] = user.remember_token
+  end
+  
+  # 記憶トークンcookieに対応するユーザーを返す
   def current_user
-    # DB への問い合わせの数を可能な限り小さくしたい
-    if session[:user_id]
-      @current_user ||= User.find_by(id: session[:user_id])
-      # @current_user = current_user || User.find_by(id: session[:user_id])
+    if (user_id = session[:user_id])
+      #user_idにsession[:user_id]を代入して、それがnilかどうかをifしている
+      @current_user ||= User.find_by(id: user_id)
+    elsif (user_id = cookies.encrypted[:user_id])
+      user = User.find_by(id: user_id)
+      if user && user.authenticated?(cookies[:remember_token])
+        log_in user
+        @current_user = user
+      end
     end
   end
+
+
+  # # 現在ログイン中のユーザーを返す（いる場合）
+  # def current_user
+  #   # DB への問い合わせの数を可能な限り小さくしたい
+  #   if session[:user_id]
+  #     @current_user ||= User.find_by(id: session[:user_id])
+  #     # @current_user = current_user || User.find_by(id: session[:user_id])
+  #   end
+  # end
   
   # ユーザーがログインしていればtrue、その他ならfalseを返す
   def logged_in?
@@ -40,5 +62,19 @@ module SessionsHelper
   def store_location
     # if request.get? => PATCHリクエストは無視する（10章前半最後）
     session[:forwarding_url] = request.original_url if request.get?
+  end
+  
+  # 永続的セッションを破棄する
+  def forget(user)
+    user.forget
+    cookies.delete(:user_id)
+    cookies.delete(:remember_token)
+  end
+
+  # 現在のユーザーをログアウトする
+  def log_out
+    forget(current_user)
+    session.delete(:user_id)
+    @current_user = nil
   end
 end
